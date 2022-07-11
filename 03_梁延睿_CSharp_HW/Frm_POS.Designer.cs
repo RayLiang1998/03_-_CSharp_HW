83c5485452fbda66c66c764e9e85c8f9a1c705b4
延睿 梁 <rayliang1998@gmail.com>
2022-07-11 18:19:53 +0800

    07111820

--- 03_梁延睿_CSharp_HW/Frm_POS.Designer.cs
+++ 03_梁延睿_CSharp_HW/Frm_POS.Designer.cs
@@ -29,6 +29,7 @@ namespace _03_梁延睿_CSharp_HW
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_POS));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -46,79 +47,94 @@ namespace _03_梁延睿_CSharp_HW
             // 
             // button1
             // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.button1.Location = new System.Drawing.Point(42, 87);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(142, 98);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Beer";
+            this.button1.Text = "Beer\r\nNT$120\r\n";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.button2.Location = new System.Drawing.Point(190, 87);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(142, 98);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Tequila";
+            this.button2.Text = "Tequila\r\nNT$180\r\n";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.button3.Location = new System.Drawing.Point(190, 191);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(142, 98);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Wine";
+            this.button3.Text = "Wine\r\nNT$320";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button4.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.button4.Location = new System.Drawing.Point(42, 191);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(142, 98);
             this.button4.TabIndex = 3;
-            this.button4.Text = "Whisky";
+            this.button4.Text = "Whisky\r\nNT$350";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.Cornsilk;
             this.button5.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button5.Location = new System.Drawing.Point(359, 87);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(151, 51);
             this.button5.TabIndex = 4;
             this.button5.Text = "by Cash";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.Color.Cornsilk;
             this.button6.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button6.Location = new System.Drawing.Point(359, 154);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(151, 51);
             this.button6.TabIndex = 5;
             this.button6.Text = "by Credit Card\r\n (10% discount)";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
+            this.button7.BackColor = System.Drawing.Color.Honeydew;
             this.button7.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button7.Location = new System.Drawing.Point(658, 326);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(107, 35);
             this.button7.TabIndex = 6;
             this.button7.Text = "clear";
-            this.button7.UseVisualStyleBackColor = true;
+            this.button7.UseVisualStyleBackColor = false;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // label1
