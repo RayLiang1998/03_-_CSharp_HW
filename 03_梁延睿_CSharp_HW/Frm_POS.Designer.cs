e68b787f56ec87cea8e65d7fa386dd222ccbfebd
延睿 梁 <rayliang1998@gmail.com>
2022-07-11 18:11:57 +0800

    07111810

--- 03_梁延睿_CSharp_HW/Frm_POS.Designer.cs
+++ 03_梁延睿_CSharp_HW/Frm_POS.Designer.cs
@@ -39,73 +39,83 @@ namespace _03_梁延睿_CSharp_HW
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button1.Location = new System.Drawing.Point(42, 87);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(142, 98);
             this.button1.TabIndex = 0;
-            this.button1.Text = "beer";
+            this.button1.Text = "Beer";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button2.Location = new System.Drawing.Point(190, 87);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(142, 98);
             this.button2.TabIndex = 1;
-            this.button2.Text = "tequila";
+            this.button2.Text = "Tequila";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button3.Location = new System.Drawing.Point(190, 191);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(142, 98);
             this.button3.TabIndex = 2;
-            this.button3.Text = "wine";
+            this.button3.Text = "Wine";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button4.Location = new System.Drawing.Point(42, 191);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(142, 98);
             this.button4.TabIndex = 3;
-            this.button4.Text = "whisly";
+            this.button4.Text = "Whisky";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button5.Location = new System.Drawing.Point(359, 87);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(151, 51);
             this.button5.TabIndex = 4;
-            this.button5.Text = "by cash";
+            this.button5.Text = "by Cash";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
+            this.button6.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button6.Location = new System.Drawing.Point(359, 154);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(151, 51);
             this.button6.TabIndex = 5;
-            this.button6.Text = "by credit card\r\n (10% discount)";
+            this.button6.Text = "by Credit Card\r\n (10% discount)";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(609, 356);
+            this.button7.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button7.Location = new System.Drawing.Point(658, 326);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(151, 51);
+            this.button7.Size = new System.Drawing.Size(107, 35);
             this.button7.TabIndex = 6;
             this.button7.Text = "clear";
             this.button7.UseVisualStyleBackColor = true;
@@ -113,21 +123,23 @@ namespace _03_梁延睿_CSharp_HW
             // 
             // label1
             // 
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(556, 88);
+            this.label1.Location = new System.Drawing.Point(538, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(203, 201);
+            this.label1.Size = new System.Drawing.Size(227, 221);
             this.label1.TabIndex = 7;
             this.label1.Text = "No Orders Yet";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(556, 299);
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(363, 257);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(202, 38);
+            this.label2.Size = new System.Drawing.Size(145, 35);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Total: ";
             // 
             // label3
             // 
@@ -139,6 +151,37 @@ namespace _03_梁延睿_CSharp_HW
             this.label3.Size = new System.Drawing.Size(309, 262);
             this.label3.TabIndex = 9;
             this.label3.Text = "Menu : ";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(350, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 170);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Payment Options : ";
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(350, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 83);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Total: ";
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(533, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(239, 262);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Shopping List :";
             // 
             // Frm_POS
             // 
@@ -155,6 +198,9 @@ namespace _03_梁延睿_CSharp_HW
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
             this.Name = "Frm_POS";
             this.Text = "Frm_POS";
             this.ResumeLayout(false);
@@ -173,5 +219,8 @@ namespace _03_梁延睿_CSharp_HW
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
